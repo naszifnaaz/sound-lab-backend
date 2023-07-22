5ef658c1e6064e484ede0063f19ae4bf5d195d3c
naszifnaaz <naszifnaaz@gmail.com>
2023-07-23 00:11:10 +0530

    ci: user-delete-and-patch-API

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,12 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
 
-// User Routes for creating and accessing users
-
 router.get("/", async (req, res) => {
   try {
     const users = await User.find().lean().exec();
-    console.log(users);
     return res.status(200).send(users);
   } catch (err) {
     return res.status(500).send(err);
@@ -17,9 +14,29 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
   try {
     const users = await User.create(req.body);
-    res.status(200).send({ users });
+    return res.status(200).send({ users });
   } catch (err) {
     return res.status(500).send(err);
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    return res.status(202).send({ user });
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    return res.status(202).send({ user });
+  } catch (err) {
+    return res.status(204).send(err);
   }
 });
 
